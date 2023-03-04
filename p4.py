c589a74d45bffe81d5d197aa38723c599814c02a
Darth Vader <danieltarancon@gmail.com>
2023-03-04 16:46:09 +0100

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,2 +1,14 @@
 # Target and collect all the numbers put them into a list and then get the sum
 
+def tasks04():
+    j = 0
+    with open('file_handling.txt') as f:
+        data = f.readlines()
+    for line in data:
+        if j == 3:
+            return f'\n{line}'
+        else:
+            j += 1
+            continue
+
+print(tasks04())
