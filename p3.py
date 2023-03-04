bd0b64aa3810323144c349676f6706d984c999b2
Darth Vader <danieltarancon@gmail.com>
2023-03-04 16:45:09 +0100

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,6 +1,6 @@
 
 
-def tasks02():
+def tasks03():
     j = 0
     with open('file_handling.txt') as f:
         data = f.readlines()
@@ -11,5 +11,24 @@ def tasks02():
             j += 1
             continue
 
-print(tasks02())
+print(tasks03())
+
+# open the binary file for reading
+with open('binary.bin', 'rb') as f:
+    # read the contents of the binary file
+    binary_data = f.read()
+
+# convert the binary data to a hexadecimal string
+hex_string = binary_data.hex()
+
+# open a text file for writing
+with open('hex_file.txt', 'w') as f:
+    # write the hexadecimal string to the text file
+    f.write(hex_string)
+
+
+
+
+
+
 
