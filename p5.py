7f35d62f372b0b2572b1f050f4d9d2983bff205d
Darth Vader <danieltarancon@gmail.com>
2023-03-04 17:48:43 +0100

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,14 +1,19 @@
 #target blank rows and remove them modifying the original file
 
-# create a string with non-ASCII characters
-text = "こんにちは世界"
 
-# encode the string to UTF-8
-utf8_bytes = text.encode('utf-8')
 
-# decode the bytes back to a string
-decoded_text = utf8_bytes.decode('utf-8')
+import re
 
-# print the original string and the decoded string
-print("Original string: ", text)
-print("Decoded string: ", decoded_text)
+pattern_blank = r'^[\s]+$'
+
+with open('testp5.txt') as f:
+    lines = f.readlines()
+
+non_blank_lines = []
+
+for line in lines:
+    if not re.match(pattern_blank, line):
+        non_blank_lines.append(line)
+
+with open('testp5.txt', 'w') as f:
+    f.writelines(non_blank_lines)
